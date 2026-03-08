9151f2ff3977d90823a3dbbfb83efd6638098168
antrikshdhand <dhandantriksh@gmail.com>
2026-03-08 15:56:42 +1100

    Create basic URL and view structure

--- main/backend/spi/urls.py
+++ main/backend/spi/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.http import HttpResponse
+
+def server_landing(request):
+    return HttpResponse("SPI backend is running.")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", server_landing, name="server_landing"),
+    path("admin/", admin.site.urls),
+    path("api/", include("core.urls")),
 ]
